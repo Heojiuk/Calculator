83b9c2c6b8c91c5a3ca6ada2b8a9b980b263b69b
Heojiuk <jwheo92@gmail.com>
2018-09-18 16:32:40 +0900

    IdConnected

--- app/src/main/java/com/example/jiukheo/calculate/MainActivity.java
+++ app/src/main/java/com/example/jiukheo/calculate/MainActivity.java
@@ -2,12 +2,58 @@ package com.example.jiukheo.calculate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    protected double res = 0.; //result
+
+    protected void inputNum(){
+        }
+
+        double co4(double A){
+
+        return 0.;
+        }
+
+        double mathOp(double x){
+
+        return 0.;
+        }
+
+        void printNum(double x){
+
+
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button PlusBtn = (Button) findViewById(R.id.PlusBtn);
+        Button SubBtn = (Button) findViewById(R.id.SubBtn);
+        Button MulBtn = (Button) findViewById(R.id.MulBtn);
+        Button DivBtn = (Button) findViewById(R.id.DivBtn);
+        Button SinBtn = (Button) findViewById(R.id.SinBtn);
+        Button CosBtn = (Button) findViewById(R.id.CosBtn);
+        Button ResultBtn = (Button) findViewById(R.id.ResultBtn);
+        Button ClearBtn = (Button) findViewById(R.id.ClearBtn);
+        Button Num1Btn = (Button) findViewById(R.id.Num1Btn);
+        Button Num2Btn = (Button) findViewById(R.id.Num2Btn);
+        Button Num3Btn = (Button) findViewById(R.id.Num3Btn);
+        Button Num4Btn = (Button) findViewById(R.id.Num4Btn);
+        Button Num5Btn = (Button) findViewById(R.id.Num5Btn);
+        Button Num6Btn = (Button) findViewById(R.id.Num6Btn);
+        Button Num7Btn = (Button) findViewById(R.id.Num7Btn);
+        Button Num8Btn = (Button) findViewById(R.id.Num8Btn);
+        Button Num9Btn = (Button) findViewById(R.id.Num9Btn);
+
+        EditText Num1 = (EditText) findViewById(R.id.Num1);
+        EditText Num2 = (EditText) findViewById(R.id.Num2);
+
+
+
+
     }
 }
